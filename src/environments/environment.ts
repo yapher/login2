676c2f1a749d547532e2e611f799fc70027c8684
andres herasimovich <andres.herasi32@gmail.com>
2018-09-27 08:50:05 -0300

    instalación y configuración de firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBNU7kH0y2lX4_MskzZE0ZNaOYjy69rRD8',
+    authDomain: 'login-2e024.firebaseapp.com',
+    databaseURL: 'https://login-2e024.firebaseio.com',
+    projectId: 'login-2e024',
+    storageBucket: 'login-2e024.appspot.com',
+    messagingSenderId: '25132199034'
+  }
 };
