debe26f9d03275445448531aa0b3f9a2adc18e67
andres herasimovich <andres.herasi32@gmail.com>
2018-09-27 01:51:54 -0300

    instalación de dependencias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // rutas
-import { HeaderComponent } from './component/pagina/header/header.component';
-import { HomeComponent } from './component/pagina/home/home.component';
-import { ListadoComponent } from './component/inquilinos/listado/listado.component';
-import { NuevoComponent } from './component/inquilinos/nuevo/nuevo.component';
-import { PdfComponent } from './component/aplicaciones/pdf/pdf.component';
-import { NewPersonComponent } from './component/inquilinos/new-person/new-person.component';
+
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'header', component: HeaderComponent},
-  {path: 'listado', component: ListadoComponent},
-  {path: 'new-person', component: NewPersonComponent},
-  {path: 'nuevo', component: NuevoComponent},
-  {path: 'pdf', component: PdfComponent}
+  // {path: '', component: HomeComponent},
+  // {path: 'header', component: HeaderComponent},
+  // {path: 'listado', component: ListadoComponent},
+  // {path: 'new-person', component: NewPersonComponent},
+  // {path: 'nuevo', component: NuevoComponent},
+  // {path: 'pdf', component: PdfComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,51 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-//rutas
+// rutas
 import { AppRoutingModule } from './app-routing.module';
 
-//modulos
+// modulos
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './component/pagina/header/header.component';
-import { HomeComponent } from './component/pagina/home/home.component';
-import { ListadoComponent } from './component/inquilinos/listado/listado.component';
-import { NuevoComponent } from './component/inquilinos/nuevo/nuevo.component';
-import { AplicComponent } from './component/pagina/aplic/aplic.component';
 import { MyDatePickerModule } from 'mydatepicker';
 
-//servicios
+// servicios
 import { ServiceInquilinoService } from './services/service-inquilino.service';
 
-//ajax
+// ajax
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
-
-
-// buscadores
-
-import {FiltroInquilinosPipe} from './component/inquilinos/filtrarInquilino';
-
-
-//google maps
-import { AgmCoreModule } from '@agm/core';
-import { Submenu1Component } from './component/pagina/submenu1/submenu1.component';
-import { PdfComponent } from './component/aplicaciones/pdf/pdf.component';
-import { PdfViewerModule } from 'ng2-pdf-viewer';
-import { NewPersonComponent } from './component/inquilinos/new-person/new-person.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FiltroInquilinosPipe,
-    HeaderComponent,
-    HomeComponent,
-    ListadoComponent,
-    NuevoComponent,
-    AplicComponent,
-    Submenu1Component,
-    PdfComponent,
-    NewPersonComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -55,11 +28,7 @@ import { NewPersonComponent } from './component/inquilinos/new-person/new-person
     AppRoutingModule,
     HttpModule,
     FormsModule,
-    PdfViewerModule,
-    SlimLoadingBarModule.forRoot(),
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyAVA8B_hlnYoHWH6I0FtcuMIFwDifHHTe0'
-    })
+    SlimLoadingBarModule.forRoot()
   ],
   providers: [ServiceInquilinoService],
   bootstrap: [AppComponent]
